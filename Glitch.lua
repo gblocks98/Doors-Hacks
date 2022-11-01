936d107ace590d5af5da31fcba80cbbb2d22d397
gblocks98 <117234767+gblocks98@users.noreply.github.com>
2022-11-01 19:08:35 -0400

    Create Glitch.lua

---
+++ Glitch.lua
@@ -0,0 +1,1 @@
+require(game.ReplicatedStorage.ClientModules.EntityModules.Glitch).stuff(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game), workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
